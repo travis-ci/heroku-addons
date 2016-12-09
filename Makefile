776efa1d7b546690a9ab4ffabf030cba4622ea1e
Buck Doyle <buck@travis-ci.org>
2016-12-08 21:17:20 -0500

    Add Travis SSO on Heroku via makey-go

---
+++ Makefile
@@ -0,0 +1,4 @@
+heroku:
+	wget -q https://github.com/travis-ci/sso/releases/download/v0.1.9/sso
+	chmod +x sso
+	mv sso bin
--- config/prod.exs
+++ config/prod.exs
@@ -1,9 +1,8 @@
 use Mix.Config
 
 config :meta_dashboard, MetaDashboard.Endpoint,
-  http: [port: {:system, "PORT"}],
+  http: [port: 4000],
   url: [host: "travis-meta-dashboard.herokuapp.com", port: 443],
-  force_ssl: [rewrite_on: [:x_forwarded_proto]],
   cache_static_manifest: "priv/static/manifest.json",
   secret_key_base: System.get_env("SECRET_KEY_BASE")
 
