5eef6bb5c00107e3b99ee30c9197c40ff5ef8d97
Buck Doyle <buck@travis-ci.org>
2016-12-09 10:36:37 -0500

    Convert listing to a table

--- test/integration/addons_test.exs
+++ test/integration/addons_test.exs
@@ -10,22 +10,25 @@ defmodule MetaDashboard.Integration.Addons do
   hound_session
 
   test "registering" do
-    with_mock Heroku.Addon, [list_existing_addons_for_an_app: fn(app) -> {:ok, [%{"name" => "#{app}-addon-one", "sso_url" => "https://heroku.com/addons/#{app}-addon-one"}, %{"name" => "#{app}-addon-two", "sso_url" => "https://heroku.com/addons/#{app}-addon-two"}]} end] do
+    with_mock Heroku.Addon, [list_existing_addons_for_an_app: fn(app) ->
+      {:ok, [
+        %{"name" => "#{app}-addon-one", "sso_url" => "https://heroku.com/addons/#{app}-addon-one"},
+        %{"name" => "#{app}-addon-two", "sso_url" => "https://heroku.com/addons/#{app}-addon-two"}
+      ]}
+    end] do
       navigate_to "/"
 
-      assert visible_text({:css, ".app:nth-child(1) > .name"}) == "travis-production"
+      assert visible_text({:css, ".app[data-name=travis-production] .name"}) == "travis-production"
 
-      assert visible_text({:css, ".app:nth-child(1) .addon:nth-child(1) a.external"}) == "travis-production-addon-one"
-      assert attribute_value({:css, ".app:nth-child(1) .addon:nth-child(1) a.external"}, "href") == "https://heroku.com/addons/travis-production-addon-one"
-      assert String.ends_with?(attribute_value({:css, ".app:nth-child(1) .addon:nth-child(1) a.shortcut"}, "href"), "/travis-production/travis-production-addon-one")
+      assert visible_text({:css, ".addon[data-app=travis-production][data-name=travis-production-addon-one] a.external"}) == "travis-production-addon-one"
+      assert attribute_value({:css, ".addon[data-app=travis-production][data-name=travis-production-addon-one] a.external"}, "href") == "https://heroku.com/addons/travis-production-addon-one"
+      assert String.ends_with?(attribute_value({:css, ".addon[data-app=travis-production][data-name=travis-production-addon-one] a.shortcut"}, "href"), "/travis-production/travis-production-addon-one")
 
-      assert visible_text({:css, ".app:nth-child(1) .addon:nth-child(2) a.external"}) == "travis-production-addon-two"
-      assert attribute_value({:css, ".app:nth-child(1) .addon:nth-child(2) a.external"}, "href") == "https://heroku.com/addons/travis-production-addon-two"
-      assert String.ends_with?(attribute_value({:css, ".app:nth-child(1) .addon:nth-child(2) a.shortcut"}, "href"), "/travis-production/travis-production-addon-two")
+      assert visible_text({:css, ".addon[data-app=travis-production][data-name=travis-production-addon-two] a.external"}) == "travis-production-addon-two"
+      assert attribute_value({:css, ".addon[data-app=travis-production][data-name=travis-production-addon-two] a.external"}, "href") == "https://heroku.com/addons/travis-production-addon-two"
+      assert String.ends_with?(attribute_value({:css, ".addon[data-app=travis-production][data-name=travis-production-addon-two] a.shortcut"}, "href"), "/travis-production/travis-production-addon-two")
 
-      assert visible_text({:css, ".app:nth-child(2) > .name"}) == "travis-pro-production"
-      assert visible_text({:css, ".app:nth-child(2) .addon:nth-child(1) a"}) == "travis-pro-production-addon-one"
-      assert visible_text({:css, ".app:nth-child(2) .addon:nth-child(2) a"}) == "travis-pro-production-addon-two"
+      assert visible_text({:css, ".app[data-name=travis-pro-production] .name"}) == "travis-pro-production"
     end
   end
 end
