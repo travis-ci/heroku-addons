b3c0fab3659f37a948d87910e810f6b6b5ed5e1d
Buck Doyle <buck@travis-ci.org>
2016-12-09 10:09:11 -0500

    Remove index test
    This is covered in the integration test.

--- test/controllers/dashboard_controller_test.exs
+++ test/controllers/dashboard_controller_test.exs
@@ -3,20 +3,6 @@ defmodule MetaDashboard.DashboardControllerTest do
 
   import Mock
 
-  test "GET /", %{conn: conn} do
-    with_mock Heroku.Addon, [list_existing_addons_for_an_app: fn(app) -> {:ok, [%{"name" => "#{app}-addon-one"}, %{"name" => "#{app}-addon-two"}]} end] do
-      conn = get conn, "/"
-
-      assert html_response(conn, 200) =~ "travis-production"
-      assert html_response(conn, 200) =~ "travis-production-addon-one"
-      assert html_response(conn, 200) =~ "travis-production-addon-two"
-
-      assert html_response(conn, 200) =~ "travis-pro-production"
-      assert html_response(conn, 200) =~ "travis-pro-production-addon-one"
-      assert html_response(conn, 200) =~ "travis-pro-production-addon-two"
-    end
-  end
-
   test "GET /app/addon-name", %{conn: conn} do
     with_mock Heroku.Addon, [list_existing_addons_for_an_app: fn(app) -> {:ok, [%{"name" => "addon-name", "sso_url" => "http://example.com"}, %{"name" => "another"}]} end] do
       conn = get conn, "/app/addon-name"
