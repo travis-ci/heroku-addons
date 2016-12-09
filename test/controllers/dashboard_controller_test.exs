9d71835973f48788541ac13143f3e3f15baec47d
Buck Doyle <buck@travis-ci.org>
2016-12-09 10:20:34 -0500

    Remove unused variable

--- test/controllers/dashboard_controller_test.exs
+++ test/controllers/dashboard_controller_test.exs
@@ -4,7 +4,7 @@ defmodule MetaDashboard.DashboardControllerTest do
   import Mock
 
   test "GET /app/addon-name", %{conn: conn} do
-    with_mock Heroku.Addon, [list_existing_addons_for_an_app: fn(app) -> {:ok, [%{"name" => "addon-name", "sso_url" => "http://example.com"}, %{"name" => "another"}]} end] do
+    with_mock Heroku.Addon, [list_existing_addons_for_an_app: fn(_) -> {:ok, [%{"name" => "addon-name", "sso_url" => "http://example.com"}, %{"name" => "another"}]} end] do
       conn = get conn, "/app/addon-name"
 
       assert redirected_to(conn) == "http://example.com"
