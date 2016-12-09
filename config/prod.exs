6393ceeec108cc2bb9b102254b656a0c99d3d364
Buck Doyle <buck@travis-ci.org>
2016-12-09 07:56:55 -0500

    Update host

--- config/prod.exs
+++ config/prod.exs
@@ -2,7 +2,7 @@ use Mix.Config
 
 config :meta_dashboard, MetaDashboard.Endpoint,
   http: [port: 4000],
-  url: [host: "travis-meta-dashboard.herokuapp.com", port: 443],
+  url: [host: "meta-dashboard.travis-ci.org", port: 443],
   cache_static_manifest: "priv/static/manifest.json",
   secret_key_base: System.get_env("SECRET_KEY_BASE")
 
