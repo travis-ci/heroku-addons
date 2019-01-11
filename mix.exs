438e9b4ca47b806df201c9c650a1d6cac13c489e
Buck Doyle <buck@travis-ci.org>
2019-01-11 11:13:58 -0600

    Update to Elixir 1.7

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule HerokuAddons.Mixfile do
     [
       app: :heroku_addons,
       version: "0.0.1",
-      elixir: "~> 1.2",
+      elixir: "~> 1.7",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: [:phoenix, :gettext] ++ Mix.compilers(),
       build_embedded: Mix.env() == :prod,
